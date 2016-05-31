5e61c830777210504c43b6a69286d3b9046e9f45
freude <freude@freudes-macbook-air.local>
2016-05-31 14:27:51 +0300

    linked list: first commit

--- LL/main.cpp
+++ LL/main.cpp
@@ -5,8 +5,35 @@
 //  Created by freude on 5/31/16.
 //  Copyright © 2016 freude. All rights reserved.
 //
+//  Linked List if integers
+//
 
 #include <iostream>
+
+
+class Node{
+public:
+    Node(){next=0;};
+    Node(int info, Node* next=0) : next(next), info(info) {};
+    int info;
+    Node* next=0;
+};
+
+class LList{
+
+public:
+    LList(){};
+    ~LList(){};
+    void AddToHead(int a);
+    void AddToTail(int a);
+    int RemoveFromHead(void);
+    int RemoveFromTail(void);
+    int RemoveItem(int a);
+    bool IsInList(int a);
+    
+    Node* head, tail;
+};
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
